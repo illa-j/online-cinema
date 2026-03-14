d78f08fd74b053b6576a86fbe5ec0736bea6e6c0
illa-j <iliacdmn@gmail.com>
2026-03-14 14:44:48 +0200

    feat: add schemas for accounts models

---
+++ src/schemas/__init__.py
@@ -0,0 +1,12 @@
+from schemas.accounts import (
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema,
+    PasswordResetRequestSchema,
+    PasswordResetCompleteRequestSchema,
+    UserLoginRequestSchema,
+    UserLoginResponseSchema,
+    UserActivationRequestSchema,
+    MessageResponseSchema,
+    TokenRefreshRequestSchema,
+    TokenRefreshResponseSchema,
+)
---
+++ src/schemas/accounts.py
@@ -0,0 +1,67 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+from database import validate_password_strength, validate_email
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {"from_attributes": True}
+
+    @field_validator("email")
+    @classmethod
+    def normalize_email_address(cls, value):
+        return validate_email(value)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        return validate_password_strength(value)
+
+
+class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(BaseEmailPasswordSchema):
+    token: str
+
+
+class UserLoginRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = {"from_attributes": True}
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
